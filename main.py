19752e29b6b71a0afc1b4e43a148608ab558ad64
Thabiso <thabisoxulu4@gmail.com>
2025-04-23 01:02:49 +0200

    Update main.py
    changed from GUI to  CLI becuase itll defeat my purpose

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-#This imports everthing in tkinter
-from tkinter import *
+introduction = print("Welcome to StreamGen!")
 
-root = Tk() # creates my window
+name = input('what is your name? ')
+age = input('How old are you? ')
+print(f'Hello {name}, Im happy to help! ')
 
-x = Label(root, text="Welcome to StreamGen!")
+print(f'{name} how are you feeling today' )
+moods = ['happy', 'Sad', "bored", 'enegetic', 'numb', "angry"]
+enter_mood = input(f'What is your mood? ')
+for mood in moods:
+    if mood == enter_mood :
+        print("what genre do you feel like Watching? ")
 
-x.pack()
-
-
-
-root.mainloop()
-
+genres = ['']
 
