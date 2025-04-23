1b7db898aadb33c0e5112898bf415ff13c422767
Thabiso <thabisoxulu4@gmail.com>
2025-04-23 04:01:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ for mood in moods:
     if mood == enter_mood :
         print("what genre do you feel like Watching? ")
 
-genres = ['Action', "Adventure", "Animation", "Biography", "Comedy", "Crime", "Cyberpunk",
-        "Documentary", "Drama", "Epic", "Experimental", "Family", "fantasy",
-        "Film Noir", "Found Footage", "Historical", "Horror", 'Isekai', 'Kids', "Martial Arts"
-        "Mecha", "Melodrama", 'Mockumentary', "Musical", "Mystery", 'NeoNior', 'Philosophical'
-        'Political', "Post Apocalyptic", 'Psychological', 'Romance', "Satire", "School Life",
-        'Science Fiction', "Sports", "Spy", 'Super Hero', 'Super natural', "Surreal", "Survival"
+genres = ['Action', "Adventure", "Animation", "Biography", "Comedy", "Crime",
+        "Documentary", "Drama", "Epic",  "Family", "fantasy",
+        "Historical", "Horror",  'Kids', "Martial Arts"
+        "Melodrama", "Musical", "Mystery",  'Philosophical',
+        'Political', "Post Apocalyptic", 'Psychological', 'Romance', "Satire", 
+        'Science Fiction', "Sports", "Spy", 'Super Hero', 'Super Natural', "Surreal", "Survival"
         "Thriller", "Tragedy", "War", 'Western']
 print(genres)
 enter_genre = input('What genre do you want to watch? ')
@@ -48,4 +48,4 @@ else:
 
 time = input("Time in minute: ")
 
-profile_map = {("Happy", "Action"): "Happy trigger"}
+profile_map = {("Happy", "Action"): "Happy trigger", ("Happy", "Adventure"): "Joyful Explorer", ("Happy", "Animation"): "Magic Wander", ("Happy", "Biography"): "Happy Listener", ("Happy", "Comedy"): "LOL", ("Happy", "Crime"): "Smiley Sleuth", ("Happy", "Documentary"): "Sunny Reels", ("Happy", "Drama"): "Golden Echoes",("Happy", "Epic"): "Joybound Legend", ("Happy", "Family"): "Heart & Harmony",("Happy", "Fantasy"): "WhimsyRealm", ("Happy", "Historical"): "Timeless Smiles",("Happy", "Horror"): "Giggles In The Dark!", ("Happy", "Kids"): "Giggle galaxy", ("Happy", "Martial Arts"): "Joyful Fury", ("Happy", "Melodrama"): "Tears of joy", ("Happy", "Musical"): "Melody Of Smiles", ("Happy", "Mystery"): "The Cheerful Clue", ("Happy", "Philosophical"): "Joyful Reflections", ("Happy", "Political"): "Optimistic Agenda",("Happy", "Post Apocalyptic"): "Sunshine after Ashes", ("Happy", "Psychological"): "Mindful Joy", ("Happy", "Romance"): "Heartful Harmony", ("Happy", "Sattire"): "Smiles & Sarcasm", ("Happy", "Science fiction"): "Stellar smiles", ("Happy", "Sports"): "Victory Vibes", ("Happy", "Spy"): "Secret Smiles", ("Happy", "Super Hero"): "Radiant Hero", ("Happy", "Super Natural"): "Magical bliss", ("Happy", "Surreal"): "Dreamlit Delight", ("Happy", "Survival"): "Survival's Smile", ("Happy", "Thriller"): "Thrills and Chills", ("Happy", "Tragedy"): "Tear Of Hope", ("Happy", "War"): "Victory In Peace", ("Happy", "Western"): "Sunny Trails", }
